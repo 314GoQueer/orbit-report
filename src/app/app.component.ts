70c3897bf6bf841023a8c61019b2e2658ceb9918
314GoQueer <314goqueer@gmail.com>
2019-10-29 14:37:01 -0500

    implement Searching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,12 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
-
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
+    this.displayList = this.sourceList;
+
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -30,4 +32,18 @@ export class AppComponent {
        }.bind(this));
     }.bind(this));
  }
+ search(): void {
+  let searchTerm = document.querySelector('input').value;
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+  }
 }
